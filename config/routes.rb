3e035a286c1402ad3fffc1357eecfc83dcf70a17
Paarroo <code_source_0@protonmail.com>
2025-07-14 19:19:55 +0200

    zoldciksqhcsqdikojchgsdqiouchz_controller & models rollsback

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'pages#home'
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :movies do
-     resources :reviews, except: [:index]
-   end
+    resources :reviews, except: [ :index ]
+  end
 
-   resources :events do
-     resources :participations, only: [:create, :destroy]
-   end
+  resources :events do
+    resources :participations, only: [ :create, :destroy ]
+  end
 
-   resources :creators
-   resources :participations, only: [:index, :show]
-   resources :reviews, only: [:index, :show]
- end
+  resources :creators
+  resources :participations, only: [ :index, :show ]
+  resources :reviews, only: [ :index, :show ]
+
   get "up" => "rails/health#show", as: :rails_health_check
-
 end

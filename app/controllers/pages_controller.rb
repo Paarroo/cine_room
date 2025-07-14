997f46cf8d9c046ac1deca1705a006f3fdf99bef
flosteno <florian-benoit@outlook.fr>
2025-07-14 12:01:56 +0200

    Created variable creators in home method of pagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+
   def home
+    @creators = Movie
+      .where.not(director: [nil, ""])
+      .group(:director)
+      .select("director, COUNT(*) AS movie_count")
+      .order("movie_count DESC")
   end
+
 end

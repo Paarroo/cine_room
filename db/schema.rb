a02f723f966e74d966bf3c701ac7ad50e11f1ad1
Drixyz13 <mattmariller@gmail.com>
2025-07-15 19:17:39 +0200

    Add seats column to participations for multi-seat booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_15_092704) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_15_171414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -75,6 +75,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_15_092704) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seats", default: 1, null: false
     t.index ["event_id"], name: "index_participations_on_event_id"
     t.index ["status"], name: "index_participations_on_status"
     t.index ["user_id", "event_id"], name: "index_participations_on_user_id_and_event_id", unique: true

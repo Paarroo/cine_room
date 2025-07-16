c143ad1a9965f51f3a2b8dc59b171f684cd7d8f0
Paarroo <code_source_0@protonmail.com>
2025-07-16 23:31:47 +0200

    all safe

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,13 @@
 class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable,
+           :recoverable, :rememberable, :validatable
+
+    enum :status, {
+       inactive: 0,
+       pending: 1,
+       active: 2,
+       super_admin: 3
+     }
 end

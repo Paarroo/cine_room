4af1c5770219281c29ae9690e3be7c2fc1d06dca
Paarroo <code_source_0@protonmail.com>
2025-07-16 16:34:59 +0200

    admin routes finished all set ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :admin_users
   devise_for :users
 
   root 'pages#home'
-
 
   get '/contact', to: 'pages#contact', as: :contact
   get '/legal',   to: 'pages#legal',   as: :legal
   get '/privacy', to: 'pages#privacy', as: :privacy
   get '/terms',   to: 'pages#terms',   as: :terms
-
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  get '/about',   to: 'pages#about',   as: :about
 
   resources :movies do
     resources :reviews, except: [ :index ]
@@ -28,6 +27,7 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-
   get "up" => "rails/health#show", as: :rails_health_check
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 end

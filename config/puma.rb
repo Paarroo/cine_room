03848bd25051d214aef79db25bf7f1939a274cc5
Paarroo <code_source_0@protonmail.com>
2025-07-17 09:17:49 +0200

    database + puma on heroku doc

--- config/puma.rb
+++ config/puma.rb
@@ -58,18 +58,25 @@ pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
 
 
 # Config Heroku
-if ENV["RAILS_ENV"] == "production"
-  preload_app!
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-  # NB workers Heroku
-  worker_count = ENV.fetch("WEB_CONCURRENCY") { 1 }
-  workers worker_count if worker_count.to_i > 1
+preload_app!
 
-  before_fork do
-    ActiveRecord::Base.connection.disconnect! if defined?(ActiveRecord)
-  end
+# Support IPv6 by binding to host `::` instead of `0.0.0.0`
+port(ENV['PORT'] || 3000, "::")
 
-  on_worker_boot do
-    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+# Turn off keepalive support for better long tails response time with Router 2.0
+# Remove this line when https://github.com/puma/puma/issues/3487 is closed, and the fix is released
+enable_keep_alives(false) if respond_to?(:enable_keep_alives)
+
+rackup      DefaultRackup if defined?(DefaultRackup)
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker-specific setup for Rails 4.1 to 5.2, after 5.2 it's not needed
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
   end
 end

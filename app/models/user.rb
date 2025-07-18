1c616d4ccb2a5d4a1cdb48fe0b02945f831aab2b
Paarroo <code_source_0@protonmail.com>
2025-07-18 14:52:25 +0200

    admin parameters added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
 
   after_create :send_welcome_email
 
+  has_one :creator, dependent: :destroy
   has_many :participations, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :events, through: :participations
   has_one :creator, dependent: :destroy
+  has_many :validated_movies, class_name: 'Movie', foreign_key: 'validated_by_id'
 
 
   enum :role, { user: 0, creator: 1, admin: 2 }, default: :user
+  validates :email, presence: true, uniqueness: true
+    validates :role, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
@@ -18,7 +22,19 @@ class User < ApplicationRecord
 
   private
 
-  def send_welcome_email
-    UserMailer.welcome_email(self).deliver_later
-  end
+  def display_name
+      full_name.present? ? full_name : email
+    end
+
+    def admin?
+      role == 'admin'
+    end
+
+    def can_access_admin?
+      admin?
+    end
+
+    def send_welcome_email
+      UserMailer.welcome_email(self).deliver_later
+    end
 end

cb6653d1916a610af4de4bc8bce3cca959176b23
flosteno <florian-benoit@outlook.fr>
2025-07-19 14:16:53 +0200

    Changed db structure in relation with user and movie

--- db/migrate/20250715171414_add_seats_to_participations.rb
+++
@@ -1,8 +0,0 @@
-class ChangeCreatorIdToUserIdInMovies < ActiveRecord::Migration[8.0]
-  def change
-    rename_column :movies, :creator_id, :user_id
-
-    remove_foreign_key :movies, :creators if foreign_key_exists?(:movies, :creators)
-    add_foreign_key :movies, :users, column: :user_id
-  end
-end

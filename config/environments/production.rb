cc17117818b45e641c3e5ae55bad2bf79e8e7b16
Paarroo <code_source_0@protonmail.com>
2025-07-22 12:20:02 +0200

    Revert "setting for mailer in prod"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,14 +101,13 @@ Rails.application.configure do
   # }
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "votre_domaine.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com", port: 587, domain: "gmail.com",
+      authentication: "plain", enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"], password: ENV["GMAIL_PASSWORD"]
+    }
+    puts "📧 SMTP Gmail"
+  
+  
 end

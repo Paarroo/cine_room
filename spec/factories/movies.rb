d21f6189eafdc4737ae9fefe957589c44db909fb
flosteno <florian-benoit@outlook.fr>
2025-07-22 20:56:36 +0200

    Updated seed

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,16 +1,42 @@
+
+
 FactoryBot.define do
   factory :movie do
     title { Faker::Movie.title }
     synopsis { Faker::Lorem.paragraph(sentence_count: 5) }
     director { Faker::Name.name }
     duration { rand(80..180) }
-    genre { [ 'Drame', 'Comédie', 'Thriller', 'Documentaire', 'Science-Fiction' ].sample }
-    language { [ 'fr', 'en', 'es' ].sample }
+    genre { %w[Drame Comédie Thriller Documentaire Science-Fiction].sample }
+    language { %w[fr en es].sample }
     year { rand(2015..2024) }
     trailer_url { "https://www.youtube.com/watch?v=#{Faker::Alphanumeric.alpha(number: 11)}" }
-    poster_url { Faker::LoremFlickr.image(size: "300x450", search_terms: [ 'movie' ]) }
     validation_status { :pending }
 
-    association :user, factory: [ :user, :creator ]  # <- CHANGEMENT ICI
+    association :user, factory: %i[user creator]
+
+    after(:build) do |movie|
+      movie.authorship_confirmed = "1"
+
+      urls = [
+        "https://source.unsplash.com/300x450/?cinema",
+        "https://source.unsplash.com/300x450/?movie-poster",
+        "https://source.unsplash.com/300x450/?film",
+        
+      ]
+
+      # Génère un poster aléatoire depuis Unsplash
+      begin
+        file = URI.open(urls.sample)
+      rescue OpenURI::HTTPError => e
+        puts "⚠️ Poster download failed: #{e.message}"
+        file = File.open(Rails.root.join("spec/fixtures/files/test-poster.jpg")) # fallback
+      end
+
+      movie.poster.attach(
+        io: file,
+        filename: "poster.jpg",
+        content_type: "image/jpeg"
+      )
+    end
   end
 end

75768007d2a01c63a9df4d0a547d55dda42b3039
Paarroo <code_source_0@protonmail.com>
2025-07-22 09:47:52 +0200

    setting for mailer in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,13 +101,14 @@ Rails.application.configure do
   # }
 
 
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com", port: 587, domain: "gmail.com",
-      authentication: "plain", enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"], password: ENV["GMAIL_PASSWORD"]
-    }
-    puts "📧 SMTP Gmail"
-  
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "votre_domaine.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end

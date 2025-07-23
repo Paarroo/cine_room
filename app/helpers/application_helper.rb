abd1b70df0b9fbe1ae92be67f61f8d607cb9fdb4
Paarroo <code_source_0@protonmail.com>
2025-07-23 02:39:11 +0200

    application helper + movies helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
 module ApplicationHelper
+  def safe_id(object, default = 0)
+    object&.id || default
+  end
+
+  def safe_attr(object, attribute, default = '')
+    return default if object.nil?
+    object.respond_to?(attribute) ? object.send(attribute) : default
+  end
+
+  def safe_render(partial, locals = {})
+    object = locals[:object] || locals.values.first
+    return '' if object.nil?
+    render partial, locals
+  rescue => e
+    Rails.logger.error "Safe render error: #{e.message}"
+    ''
+  end
+
+  def safe_each(collection, &block)
+    return [] if collection.nil? || !collection.respond_to?(:each)
+    collection.compact.each(&block)
+  end
+
+  def safe_count(collection)
+    return 0 if collection.nil?
+    collection.respond_to?(:count) ? collection.count : 0
+  end
+
+  def safe_present?(object)
+    object&.present? || false
+  end
 end

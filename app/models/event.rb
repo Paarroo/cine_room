23929edd443e3e431361336d15e0abb68ab5192e
flosteno <florian-benoit@outlook.fr>
2025-07-24 16:49:32 +0200

    Added gem whenever and configured auto change of event status to finished

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  before_save :auto_set_finished_status
+  before_save :update_status_if_sold_out
+
   belongs_to :movie
   has_many :participations, dependent: :destroy
   has_many :users, through: :participations
@@ -7,10 +10,12 @@ class Event < ApplicationRecord
   validates :title, :venue_name, :venue_address, :event_date, :start_time, :max_capacity, :price_cents, presence: true
   validates :max_capacity, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
   validates :price_cents, numericality: { greater_than: 0 }
+  validate :event_date_must_be_at_least_one_week_from_now
 
+  
   enum :status, { upcoming: 0, sold_out: 1, ongoing: 2, finished: 3, cancelled: 4 }, default: :upcoming
 
-  before_save :update_status_if_sold_out
+  
 
   scope :by_title, ->(q) {
     joins(:movie).where("movies.title ILIKE ?", "%#{q}%") if q.present?
@@ -42,6 +47,17 @@ class Event < ApplicationRecord
       .by_date_filter(params[:date_filter])
   end
 
+  def self.update_finished_events
+    where("event_date < ?", Time.current).where.not(status: :finished).update_all(status: :finished)
+  end
+
+  def event_date_must_be_at_least_one_week_from_now
+    if event_date < 1.week.from_now
+      errors.add(:event_date, "doit être au moins une semaine après aujourd’hui")
+    end
+  end
+
+
   def available_spots
     reserved_seats = participations.where(status: 'confirmed').sum(:seats)
     max_capacity - reserved_seats
@@ -67,6 +83,12 @@ class Event < ApplicationRecord
 
   def update_status_if_sold_out
     self.status = 'sold_out' if sold_out? && upcoming?
+  end
+
+  def auto_set_finished_status
+    if event_date.present? && event_date < Time.current
+      self.status = "finished"
+    end
   end
 
   scope :upcoming, -> { where(status: :upcoming) }

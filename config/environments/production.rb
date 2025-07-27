161d1875699d3644edc2aa2c0b2442dd1b3b7a74
Paarroo <code_source_0@protonmail.com>
2025-07-27 14:21:46 +0200

    fix: configure SolidQueue for ActiveJob in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,10 +70,9 @@ Rails.application.configure do
   }
 
 
-  # Temporary fix: use inline delivery for emails instead of SolidQueue
-  config.active_job.queue_adapter = :inline
-  # config.active_job.queue_adapter = :solid_queue
-  # config.solid_queue.connects_to = { database: { writing: :queue } }
+  # Use SolidQueue for background jobs
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

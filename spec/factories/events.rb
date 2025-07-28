25bb528e321e4164541b9a5035b864ba712b67cf
Paarroo <code_source_0@protonmail.com>
2025-07-28 08:31:00 +0200

    fix: resolve geocoding issues with realistic French addresses
    - Replace Faker fake addresses with real Paris cinema venues
    - Fix existing events with correct addresses and regeocoding
    - Add address format validation to prevent fake addresses
    - Create rake task to fix existing geocoding issues
    - Improve geocoding logging and error handling
    - All events now have accurate coordinates matching their addresses

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,15 +3,31 @@ FactoryBot.define do
     association :movie, factory: [:movie, :validated]
     title { "Projection de #{movie.title}" }
     description { Faker::Lorem.paragraph(sentence_count: 3) }
-    venue_name { Faker::Company.name + " Cinema" }
-    venue_address { Faker::Address.full_address }
+    venue_name { ["Cinéma Rex", "Gaumont", "UGC", "MK2", "Pathé", "Le Grand Action", "Studio des Ursulines", "Cinéma du Panthéon"].sample }
+    venue_address { 
+      [
+        "1 Boulevard Poissonnière, 75002 Paris",
+        "30 Avenue d'Italie, 75013 Paris", 
+        "14 Rue Lincoln, 75008 Paris",
+        "128 Avenue de France, 75013 Paris",
+        "19 Rue de Vaugirard, 75006 Paris",
+        "5 Rue Banquier, 75013 Paris",
+        "10 Rue des Ursulines, 75005 Paris",
+        "13 Rue Victor Cousin, 75005 Paris",
+        "22 Rue Saint-Antoine, 75004 Paris",
+        "15 Rue de la Gaîté, 75014 Paris",
+        "73 Boulevard de Strasbourg, 75010 Paris",
+        "66 Avenue des Champs-Élysées, 75008 Paris"
+      ].sample
+    }
     event_date { rand(1.week.from_now..3.months.from_now).to_date }
     start_time { [ '19:00', '20:30', '21:00', '18:30', '22:00' ].sample }
     max_capacity { rand(50..100) }
     price_cents { rand(800..2500) }
     status { [ :upcoming, :finished, :sold_out ].sample }
-    latitude { Faker::Address.latitude.to_f }
-    longitude { Faker::Address.longitude.to_f }
+    # Coordinates will be set by geocoding, removed fake ones
+    latitude { nil }
+    longitude { nil }
 
     trait :upcoming do
       status { :upcoming }
